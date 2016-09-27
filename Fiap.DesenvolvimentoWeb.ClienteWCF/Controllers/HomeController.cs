32fa2abd492c81a0c322fbaf1a25a348a50e5898
Diogo Koshiba <diogo.koshiba@gmail.com>
2016-09-26 22:45:11 -0300

    wvf

---
+++ Fiap.DesenvolvimentoWeb.ClienteWCF/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fiap.DesenvolvimentoWeb.ClienteWCF.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var servico = new Srf.Service1Client();
+            var lista = servico.ListarFuncionarios();
+
+            return View(lista);
+        }
+    }
+}
